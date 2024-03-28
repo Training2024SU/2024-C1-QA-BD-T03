6140883282a93356d92123c2165dcd08afed10e6
Luis Molina <lemolina27@hotmail.com>
2024-03-28 12:35:35 -0500

    Se añade carpeta de analisis al taller realizado por Janet Marulanda

---
+++ Janet_Marulanda/LibreriaJM.sql
@@ -0,0 +1,277 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: libreriajm
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `autor`
+--
+
+DROP TABLE IF EXISTS `autor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `autor` (
+  `codigo_autor` varchar(20) NOT NULL,
+  `Nombre_autor` varchar(100) DEFAULT NULL,
+  `Fecha_nacimiento` date DEFAULT NULL,
+  PRIMARY KEY (`codigo_autor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `autor`
+--
+
+LOCK TABLES `autor` WRITE;
+/*!40000 ALTER TABLE `autor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `autor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categoria_literaria`
+--
+
+DROP TABLE IF EXISTS `categoria_literaria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categoria_literaria` (
+  `ISBN` varchar(40) NOT NULL,
+  `nombre_categoria` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`ISBN`),
+  CONSTRAINT `categoria_literaria_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `libro` (`ISBN`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria_literaria`
+--
+
+LOCK TABLES `categoria_literaria` WRITE;
+/*!40000 ALTER TABLE `categoria_literaria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria_literaria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `Id_cliente` varchar(20) NOT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`Id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `compra`
+--
+
+DROP TABLE IF EXISTS `compra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compra` (
+  `id_compra` int NOT NULL,
+  `Id_cliente` varchar(20) DEFAULT NULL,
+  `ISBN` varchar(40) DEFAULT NULL,
+  `Fecha_compra` date DEFAULT NULL,
+  PRIMARY KEY (`id_compra`),
+  KEY `ISBN` (`ISBN`),
+  KEY `Id_cliente` (`Id_cliente`),
+  CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `libro` (`ISBN`),
+  CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`Id_cliente`) REFERENCES `cliente` (`Id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compra`
+--
+
+LOCK TABLES `compra` WRITE;
+/*!40000 ALTER TABLE `compra` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compra` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `editorial`
+--
+
+DROP TABLE IF EXISTS `editorial`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `editorial` (
+  `nombre_editorial` varchar(30) NOT NULL,
+  `direccion` varchar(150) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`nombre_editorial`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `editorial`
+--
+
+LOCK TABLES `editorial` WRITE;
+/*!40000 ALTER TABLE `editorial` DISABLE KEYS */;
+/*!40000 ALTER TABLE `editorial` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libro`
+--
+
+DROP TABLE IF EXISTS `libro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libro` (
+  `ISBN` varchar(40) NOT NULL,
+  `Titulo` varchar(150) DEFAULT NULL,
+  `anio_publicacion` year DEFAULT NULL,
+  `Numero_paginas` int DEFAULT NULL,
+  `Nombre_editorial` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`ISBN`),
+  KEY `Nombre_editorial` (`Nombre_editorial`),
+  CONSTRAINT `libro_ibfk_1` FOREIGN KEY (`Nombre_editorial`) REFERENCES `editorial` (`nombre_editorial`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libro`
+--
+
+LOCK TABLES `libro` WRITE;
+/*!40000 ALTER TABLE `libro` DISABLE KEYS */;
+/*!40000 ALTER TABLE `libro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libro_autor`
+--
+
+DROP TABLE IF EXISTS `libro_autor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libro_autor` (
+  `codigo_autor` varchar(20) DEFAULT NULL,
+  `ISBN` varchar(40) DEFAULT NULL,
+  KEY `ISBN` (`ISBN`),
+  KEY `codigo_autor` (`codigo_autor`),
+  CONSTRAINT `libro_autor_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `libro` (`ISBN`),
+  CONSTRAINT `libro_autor_ibfk_2` FOREIGN KEY (`codigo_autor`) REFERENCES `autor` (`codigo_autor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libro_autor`
+--
+
+LOCK TABLES `libro_autor` WRITE;
+/*!40000 ALTER TABLE `libro_autor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `libro_autor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nacionalidad`
+--
+
+DROP TABLE IF EXISTS `nacionalidad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nacionalidad` (
+  `codigo_autor` varchar(20) NOT NULL,
+  `Nombre_nacionalidad` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`codigo_autor`),
+  CONSTRAINT `nacionalidad_ibfk_1` FOREIGN KEY (`codigo_autor`) REFERENCES `autor` (`codigo_autor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nacionalidad`
+--
+
+LOCK TABLES `nacionalidad` WRITE;
+/*!40000 ALTER TABLE `nacionalidad` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nacionalidad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `telefono`
+--
+
+DROP TABLE IF EXISTS `telefono`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `telefono` (
+  `nombre_editorial` varchar(100) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`nombre_editorial`),
+  CONSTRAINT `telefono_ibfk_1` FOREIGN KEY (`nombre_editorial`) REFERENCES `editorial` (`nombre_editorial`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `telefono`
+--
+
+LOCK TABLES `telefono` WRITE;
+/*!40000 ALTER TABLE `telefono` DISABLE KEYS */;
+/*!40000 ALTER TABLE `telefono` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `telefono_cliente`
+--
+
+DROP TABLE IF EXISTS `telefono_cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `telefono_cliente` (
+  `id_cliente` varchar(20) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  KEY `id_cliente` (`id_cliente`),
+  CONSTRAINT `telefono_cliente_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`Id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `telefono_cliente`
+--
+
+LOCK TABLES `telefono_cliente` WRITE;
+/*!40000 ALTER TABLE `telefono_cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `telefono_cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-28 10:09:18
