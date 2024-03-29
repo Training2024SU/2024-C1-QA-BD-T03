bb8d5ce872496ee2de1c4c6fec47d2602b26ac11
Andreina <andreina.iv95@gmail.com>
2024-03-29 16:07:00 -0500

    Se reemplaza archivo PDF, y evidencias .sql

--- Solución Taller Comandos SQL/Tercera actividad/evidencia_poblar_libreria_daniel_morales.sql
+++ Solución Taller Comandos SQL/Tercera actividad/evidencia_poblar_libreria_daniel_morales.sql
@@ -1,31 +1,33 @@
-poblar_libreria_actividad2.sql
+--Instrucciones SQL para poblar la libreria_daniel_morales
 
+-- Poblado en la tabla 'editorial'
 INSERT INTO editorial (nombre, direccion, ciudad, pais, telefono) 
-VALUES ('Universe', 'Carrera 102 43 12', 'Bogota', 'Colombia', '34543343334');
+VALUES ('Universe', 'Carrera 102 43 12', 'Bogotá', 'Colombia', '34543343334');
 
+-- Poblado en la tabla 'autor'
 INSERT INTO autor (Codigo, Nombre, Nacionalidad, Fecha_nacimiento) 
 VALUES ('2', 'Stephen King', 'Estadounidense', '1947-09-21');
 
+-- Poblado en la tabla 'libro'
 INSERT INTO libro (ISBN, Titulo, Numero_paginas, Editorial_nombre, precio) 
-VALUES ('978-0670813025', 'IT', '1138', 'Planeta', '258000');
+VALUES 
+  ('978-0670813025', 'IT', '1138', 'Planeta', '258000'),
+  ('978-0198709701', 'Crimen y Castigo', '576', 'Universe', '80813');
 
-INSERT INTO libro (ISBN, Titulo, Numero_paginas, Editorial_nombre, precio) 
-VALUES ('978-0198709701', 'Crimen y Castigo', '576', 'Universe', '80813');
-
+-- Poblado en la tabla 'libro_autor'
 INSERT INTO libro_autor (ISBN_Libro, Codigo_autor) 
-VALUES ('978-0198709701', '1');
+VALUES 
+  ('978-0198709701', '1'),
+  ('978-0670813025', '2');
 
-INSERT INTO libro_autor (ISBN_Libro, Codigo_autor) 
-VALUES ('978-0670813025', '2');
-
+-- Poblado en la tabla 'cliente'
 INSERT INTO cliente (ID, Nombre) 
-VALUES ('1077234567', 'Pepito Perez Gonzalez');
+VALUES 
+  ('1077234567', 'Pepito Perez Gonzalez'),
+  ('1017321456', 'Lola Lopez');
 
-INSERT INTO cliente (ID, Nombre) 
-VALUES ('1017321456', 'Lola Lopez');
-
+-- Poblado en la tabla 'compra'
 INSERT INTO compra (ID, ID_cliente, ISBN_libro, Fecha_compra, Cantidad) 
-VALUES ('123', '1077234567', '978-0198709701', '2024-02-26 15:00:00', '1');
-
-INSERT INTO compra (ID, ID_cliente, ISBN_libro, Fecha_compra, Cantidad) 
-VALUES ('234', '1017321456', '978-0670813025', '2024-03-26 15:45:15', '1');
+VALUES 
+  ('123', '1077234567', '978-0198709701', '2024-02-26 15:00:00', '1'),
+  ('234', '1017321456', '978-0670813025', '2024-03-26 15:45:15', '1');
