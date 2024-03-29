f0aaf7787e94604fd40889d6574faf41ced3e286
Andreina <andreina.iv95@gmail.com>
2024-03-29 12:06:48 -0500

    Se mejora identacíón de las sentencias SQL

--- Solución Taller Comandos SQL/Primera actividad/Sentencias-SQL-libreria.sql
+++ Solución Taller Comandos SQL/Primera actividad/Sentencias-SQL-libreria.sql
@@ -1,48 +1,54 @@
 
--- Crear base de datos llamada "Libreria"
-
-CREATE DATABASE libreria;
+-- Crear esquema llamada "Libreria"
+CREATE SCHEMA IF NOT EXISTS `libreria_busca_libre`;
+USE  `libreria_busca_libre`
 
 -- Crear tabla libro
-CREATE TABLE `libro` (
+CREATE TABLE IF NOT EXISTS `libro` (
   `ISBN` varchar(45) NOT NULL,
-  `Nombre-editorial` varchar(45) NOT NULL,
+  `Nombre_editorial` varchar(45) NOT NULL,
   `Titulo` varchar(45) NOT NULL,
-  `Fecha-publicacion` date NOT NULL,
-  `Numero-paginas` int NOT NULL,
+  `Fecha_publicacion` date NOT NULL,
+  `Numero_paginas` int NOT NULL,
   `Precio` decimal(10,0) NOT NULL,
   PRIMARY KEY (`ISBN`),
-  KEY `Nombre_idx` (`Nombre-editorial`),
-  CONSTRAINT `Nombre` FOREIGN KEY (`Nombre-editorial`) REFERENCES `editorial` (`Nombre`)
+  KEY `Nombre_idx` (`Nombre_editorial`),
+  CONSTRAINT `Nombre` 
+  FOREIGN KEY (`Nombre_editorial`) 
+  REFERENCES `editorial` (`Nombre`)
 );
 
 -- Crear tabla autor
-CREATE TABLE `autor` (
+CREATE TABLE IF NOT EXISTS `autor` (
   `Codigo` varchar(45) NOT NULL,
   `Nombres` varchar(45) NOT NULL,
   `Apellidos` varchar(45) NOT NULL,
   `Nacionalidad` varchar(45) NOT NULL,
-  `Fecha-nacimiento` date NOT NULL,
+  `Fecha_nacimiento` date NOT NULL,
   PRIMARY KEY (`Codigo`)
 );
 
 -- Crear tabla autor-x-libro
-CREATE TABLE `autor-x-libro` (
+CREATE TABLE IF NOT EXISTS `autor_x_libro` (
   `ID-autor-libro` varchar(45) NOT NULL,
   `Codigo` varchar(45) NOT NULL,
   `ISBN` varchar(45) NOT NULL,
   `Rol` varchar(45) NOT NULL,
-  PRIMARY KEY (`ID-autor-libro`),
+  PRIMARY KEY (`ID-autor_libro`),
   KEY `Codigo-autor_idx` (`Codigo`),
   KEY `ISBN_idx` (`ISBN`),
-  CONSTRAINT `Codigo` FOREIGN KEY (`Codigo`) REFERENCES `autor` (`Codigo`),
-  CONSTRAINT `ISBN` FOREIGN KEY (`ISBN`) REFERENCES `libro` (`ISBN`)
+  CONSTRAINT `Codigo` 
+  FOREIGN KEY (`Codigo`) 
+  REFERENCES `autor` (`Codigo`),
+  CONSTRAINT `ISBN` 
+  FOREIGN KEY (`ISBN`) 
+  REFERENCES `libro` (`ISBN`)
 ); 
 
 -- Crear tabla editorial
-CREATE TABLE `editorial` (
+CREATE TABLE IF NOT EXISTS `editorial` (
   `Nombre` varchar(45) NOT NULL,
-  `Correo-electronico` varchar(45) NOT NULL,
+  `Correo_electronico` varchar(45) NOT NULL,
   `Direccion` varchar(45) NOT NULL,
   `Ciudad` varchar(45) NOT NULL,
   `Pais` varchar(45) NOT NULL,
@@ -50,42 +56,50 @@ CREATE TABLE `editorial` (
 );
 
 -- Crear tabla teléfono editorial
-CREATE TABLE `telefono-editorial` (
-  `Nombre-editorial` varchar(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `telefono_editorial` (
+  `Nombre_editorial` varchar(45) NOT NULL,
   `Numero` varchar(20) NOT NULL,
-  PRIMARY KEY (`Nombre-editorial`),
-  CONSTRAINT `Nombre-editorial` FOREIGN KEY (`Nombre-editorial`) REFERENCES `editorial` (`Nombre`)
+  PRIMARY KEY (`Nombre_editorial`),
+  CONSTRAINT `Nombre_editorial` 
+  FOREIGN KEY (`Nombre_editorial`) 
+  REFERENCES `editorial` (`Nombre`)
 );
 
 -- Crear tabla cliente
-CREATE TABLE `cliente` (
+CREATE TABLE IF NOT EXISTS `cliente` (
   `DNI` varchar(45) NOT NULL,
   `Nombres` varchar(45) NOT NULL,
   `Apellidos` varchar(45) NOT NULL,
-  `Correo-electronico` varchar(45) NOT NULL,
+  `Correo_electronico` varchar(45) NOT NULL,
   PRIMARY KEY (`DNI`)
 );
 
 -- Crear tabla teléfono cliente
-CREATE TABLE `telefono-cliente` (
-  `DNI-cliente` varchar(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `telefono_cliente` (
+  `DNI_cliente` varchar(45) NOT NULL,
   `Numero` varchar(45) NOT NULL,
-  PRIMARY KEY (`DNI-cliente`),
-  CONSTRAINT `DNI-cliente` FOREIGN KEY (`DNI-cliente`) REFERENCES `cliente` (`DNI`)
+  PRIMARY KEY (`DNI_cliente`),
+  CONSTRAINT `DNI_cliente` 
+  FOREIGN KEY (`DNI_cliente`) 
+  REFERENCES `cliente` (`DNI`)
 );
 
 -- Crear tabla compra-libros
-CREATE TABLE `compra-libros` (
+CREATE TABLE IF NOT EXISTS `compra_libros` (
   `ID-compra` int NOT NULL,
   `DNI` varchar(45) NOT NULL,
-  `Codigo-ISBN` varchar(45) NOT NULL,
-  `Fecha-compra` datetime(6) NOT NULL,
+  `Codigo_ISBN` varchar(45) NOT NULL,
+  `Fecha_compra` datetime(6) NOT NULL,
   `Cantidad` int NOT NULL,
-  `Precio-unitario` decimal(10,0) NOT NULL,
-  PRIMARY KEY (`ID-compra`),
+  `Precio_unitario` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`ID_compra`),
   KEY `DNI_idx` (`DNI`),
-  KEY `Codigo-ISBN_idx` (`Codigo-ISBN`),
-  CONSTRAINT `Codigo-ISBN` FOREIGN KEY (`Codigo-ISBN`) REFERENCES `libro` (`ISBN`),
-  CONSTRAINT `DNI` FOREIGN KEY (`DNI`) REFERENCES `cliente` (`DNI`)
+  KEY `Codigo_ISBN_idx` (`Codigo_ISBN`),
+  CONSTRAINT `Codigo_ISBN` 
+  FOREIGN KEY (`Codigo_ISBN`) 
+  REFERENCES `libro` (`ISBN`),
+  CONSTRAINT `DNI` 
+  FOREIGN KEY (`DNI`) 
+  REFERENCES `cliente` (`DNI`)
 );
 
--- Solución Taller Comandos SQL/Segunda actividad/Sentencias-Libreria-Daniel.sql
+++ Solución Taller Comandos SQL/Segunda actividad/Sentencias-Libreria-Daniel.sql
@@ -1,15 +1,16 @@
--- Crear base de datos llamada "libreria_daniel_morales"
-CREATE DATABASE libreria_daniel_morales;
+-- Crear esquema llamada "libreria_daniel_morales"
+CREATE SCHEMA IF NOT EXISTS `libreria_daniel_morales`;
+USE  `libreria_daniel_morales`
 
 -- Crear tabla Cliente
-CREATE TABLE `cliente` (
+CREATE TABLE IF NOT EXISTS `cliente` (
   `ID` varchar(30) NOT NULL,
   `Nombre` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
 );
 
 -- Crear tabla Libro
-CREATE TABLE `libro` (
+CREATE TABLE IF NOT EXISTS `libro` (
   `ISBN` varchar(20) NOT NULL,
   `Titulo` varchar(255) NOT NULL,
   `Numero_paginas` int NOT NULL,
@@ -17,11 +18,13 @@ CREATE TABLE `libro` (
   `Precio` decimal(10,0) NOT NULL,
   PRIMARY KEY (`ISBN`),
   KEY `Editorial_nombre_idx` (`Editorial_nombre`),
-  CONSTRAINT `Editorial_nombre` FOREIGN KEY (`Editorial_nombre`) REFERENCES `editorial` (`Nombre`)
+  CONSTRAINT `Editorial_nombre` 
+  FOREIGN KEY (`Editorial_nombre`) 
+  REFERENCES `editorial` (`Nombre`)
 );
 
 -- Crear tabla Compra
-CREATE TABLE `compra` (
+CREATE TABLE IF NOT EXISTS `compra` (
   `ID` int NOT NULL,
   `ID_cliente` varchar(30) NOT NULL,
   `ISBN_libro` varchar(20) NOT NULL,
@@ -30,12 +33,16 @@ CREATE TABLE `compra` (
   PRIMARY KEY (`ID`),
   KEY `ID_cliente_idx` (`ID_cliente`),
   KEY `ISBN_idx` (`ISBN_libro`),
-  CONSTRAINT `ID` FOREIGN KEY (`ID_cliente`) REFERENCES `cliente` (`ID`),
-  CONSTRAINT `ISBN` FOREIGN KEY (`ISBN_libro`) REFERENCES `libro` (`ISBN`)
+  CONSTRAINT `ID` 
+  FOREIGN KEY (`ID_cliente`) 
+  REFERENCES `cliente` (`ID`),
+  CONSTRAINT `ISBN` 
+  FOREIGN KEY (`ISBN_libro`) 
+  REFERENCES `libro` (`ISBN`)
 );
 
 -- Crear tabla Autor
-CREATE TABLE `autor` (
+CREATE TABLE IF NOT EXISTS `autor` (
   `Codigo` int NOT NULL,
   `Nombre` varchar(255) NOT NULL,
   `Nacionalidad` varchar(200) NOT NULL,
@@ -44,17 +51,21 @@ CREATE TABLE `autor` (
 );
 
 -- Crear tabla Libro_Autor
-CREATE TABLE `libro_autor` (
+CREATE TABLE IF NOT EXISTS `libro_autor` (
   `ISBN_Libro` varchar(255) NOT NULL,
   `Codigo_autor` int NOT NULL,
   PRIMARY KEY (`ISBN_Libro`),
   KEY `Codigo_autor_idx` (`Codigo_autor`),
-  CONSTRAINT `Codigo_autor` FOREIGN KEY (`Codigo_autor`) REFERENCES `autor` (`Codigo`),
-  CONSTRAINT `ISBN_libro` FOREIGN KEY (`ISBN_Libro`) REFERENCES `libro` (`ISBN`)
+  CONSTRAINT `Codigo_autor` 
+  FOREIGN KEY (`Codigo_autor`) 
+  REFERENCES `autor` (`Codigo`),
+  CONSTRAINT `ISBN_libro` 
+  FOREIGN KEY (`ISBN_Libro`) 
+  REFERENCES `libro` (`ISBN`)
 );
 
 -- Crear tabla Editorial
-CREATE TABLE `editorial` (
+CREATE TABLE IF NOT EXISTS `editorial` (
   `Nombre` varchar(255) NOT NULL,
   `Direccion` varchar(255) NOT NULL,
   `Ciudad` varchar(255) NOT NULL,
