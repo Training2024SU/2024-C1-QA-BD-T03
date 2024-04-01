7038c0fc447b08db51b00f8ae21f277acf42311a
Daniel Chaparro Cano <danielkano5@gmail.com>
2024-04-01 14:40:19 -0500

    Correciones act 2 y 3
    Se realizan correcciones actividades 2 y 3, modelamiento MER, RE, Workbench e insercción bd.

--- Actividad2/bookstore.sql
+++ Actividad2/bookstore.sql
@@ -3,17 +3,17 @@ create database bookstore;
 use bookstore;
 
 CREATE TABLE IF NOT EXISTS Autor (
-    Codigo INT NOT NULL PRIMARY KEY,
-    Nombres VARCHAR(50) not null,
-    Apellidos VARCHAR(50),
+    Codigo varchar(20) PRIMARY KEY,
+    Nombre VARCHAR(50) not null,
+    Apellido VARCHAR(50),
     Nacionalidad VARCHAR(40),
     FechaNacimiento DATE
 ) ;
 
 CREATE TABLE IF NOT EXISTS Cliente (
-    ID INT NOT NULL PRIMARY KEY,
-    Nombres VARCHAR(50) not null,
-    Apellidos VARCHAR(50),
+    ID varchar(20) PRIMARY KEY,
+    Nombre VARCHAR(50) not null,
+    Apellido VARCHAR(50),
     FechaNacimiento DATE,
     Correo varchar(100),
     Telefono varchar(20),
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS Cliente (
 CREATE TABLE IF NOT EXISTS Editorial (
     Nombre VARCHAR(50) NOT NULL PRIMARY KEY,
     Pais VARCHAR(40),
-    AnioFundacion INT,
+    AnioFundacion Date,
     Direccion VARCHAR(100),
     Correo VARCHAR(100),
     Telefono VARCHAR(20)
@@ -31,11 +31,10 @@ CREATE TABLE IF NOT EXISTS Editorial (
 
 # Crear tablas con llave foranea
 CREATE TABLE IF NOT EXISTS Libro (
-    ISBN BIGINT NOT NULL PRIMARY KEY,
+    ISBN varchar(20) PRIMARY KEY,
     Titulo VARCHAR(255) NOT NULL,
     NumeroPaginas BIGINT,
-    Precio INT,
-    Anio INT,
+    Anio Date,
     Genero VARCHAR(30),
     Sinopsis VARCHAR(255),
     EditorialNombre VARCHAR(50) NOT NULL,
@@ -44,18 +43,17 @@ CREATE TABLE IF NOT EXISTS Libro (
 ) ;
 
 CREATE TABLE IF NOT EXISTS Compra (
-    ID INT NOT NULL PRIMARY KEY,
+    ID varchar(20) PRIMARY KEY,
     FechaCompra DATE,
-    PrecioTotal BIGINT,
-    ClienteID INT not null,
+    PrecioTotal double,
+    ClienteID varchar(20),
     FOREIGN KEY (ClienteID)
         REFERENCES Cliente (ID)
 ) ;
 
-# Crear tablas intermedias de relaciones muchos a muchos
 CREATE TABLE IF NOT EXISTS Autor_Libro (
-    AutorCodigo INT NOT NULL,
-    LibroISBN BIGINT NOT NULL,
+    AutorCodigo varchar(20),
+    LibroISBN varchar(20),
     PRIMARY KEY (AutorCodigo , LibroISBN),
     FOREIGN KEY (AutorCodigo)
         REFERENCES Autor (Codigo),
@@ -63,13 +61,16 @@ CREATE TABLE IF NOT EXISTS Autor_Libro (
         REFERENCES Libro (ISBN)
 );
 
-CREATE TABLE IF NOT EXISTS Compra_Libro (
-    CompraID INT NOT NULL,
-    LibroISBN BIGINT NOT NULL,
-    Cantidad INT,
-    PRIMARY KEY (CompraID , LibroISBN),
-    FOREIGN KEY (CompraID)
-        REFERENCES Compra (ID),
-    FOREIGN KEY (LibroISBN)
-        REFERENCES Libro (ISBN)
+
+CREATE TABLE IF NOT EXISTS Copia (
+    ID varchar(20),
+    Libro_id varchar(20),
+    Id_compra varchar(20),
+    precio double,
+    PRIMARY KEY ( Libro_id  , ID),
+    FOREIGN KEY (Libro_id)
+        REFERENCES Libro (ISBN),
+    FOREIGN KEY (Id_compra)
+        REFERENCES Compra (ID)
 );
+
